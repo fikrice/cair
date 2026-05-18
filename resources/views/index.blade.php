6505329c037fb8d8b0b244c0f063a336d3d9be58
Fikri SAN <fikrice2025@gmail.com>
2026-05-18 13:44:46 +0700

    Ubah favicon menggunakan CDN

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>StockSell ERP - Transformasi Kendali Bisnis Anda</title>
-  <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v=2">
+  <link rel="icon" type="image/png" href="https://ui-avatars.com/api/?name=Mesama&background=2563eb&color=fff&rounded=true&bold=true&size=128">
   
   <!-- Premium Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
